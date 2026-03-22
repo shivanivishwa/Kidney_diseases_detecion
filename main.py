99f05c90c6998e7ee83898e6344a13feb3a0ddd0
shivanivishwa <164068861+shivanivishwa@users.noreply.github.com>
2026-03-22 14:22:45 +0530

    base model added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
-
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 logger.info("Welcome to our custom log")
 
@@ -13,4 +13,15 @@ try:
     
 except Exception as e:
         Exception(e)
+        
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
         
